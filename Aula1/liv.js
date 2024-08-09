755aa53e758514e47ab3a730944a187b328caf57
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-08-08 21:21:11 -0300

    Primeira aula de programação web

---
+++ Aula1/liv.js
@@ -0,0 +1,12 @@
+var nome = prompt('Digite seu nome ')
+console.log('Nome digitado foi: ' + nome)
+document.write('Nome digitado foi: ' + nome)
+
+ var idade = prompt('Digite a sua idade')
+document.write('A sua idade é: '  + idade) 
+console.log('A sua idade é: ' + idade)
+
+ var localizacao = prompt('Digite a sua localização')
+document.write('Você mora em: ' + localizacao)
+console.log('Você mora em: ' + localizacao)
+
---
+++ Exercicio1/li.js
@@ -0,0 +1,7 @@
+var1 = parseFloat(prompt('Digite o primeiro número: '))
+
+var2 = parseFloat(prompt('Digite o segundo número: '))
+
+let soma = var1 + var2
+document.write('Soma: ' + soma)
+document.write('<br> A Media foi: ' + soma / 2)
