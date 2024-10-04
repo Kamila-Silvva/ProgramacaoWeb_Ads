d62e9f81a0fc6a917994557dc09c3387586a6743
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-10-03 21:13:22 -0300

    iniciando React

---
+++ aula1/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Mensagem from './componets/Mensagem';
+
+function App() {
+  return (
+    <Mensagem></Mensagem>
+  );
+}
+
+export default App;
---
+++ aula1/src/componets/Mensagem.js
@@ -0,0 +1,8 @@
+function Mensagem() {
+    return (//recurso utilizado para retornar o que for programado para onde for importado o item Mensagem.js
+        <div>
+            <h1> mensagem de boas vindas !!!</h1>
+        </div>
+    );
+}
+export default Mensagem;
